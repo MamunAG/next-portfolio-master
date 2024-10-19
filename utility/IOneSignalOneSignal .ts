62a56f6449ecb075fbc88ccbce2ff7862556585d
MamunOfficeLaptop <mamun166036@gmail.com>
2024-10-19 12:38:53 +0600

    up

---
+++ utility/IOneSignalOneSignal .ts
@@ -0,0 +1,12 @@
+// interface IOneSignalOneSignal {
+// 	Slidedown: IOneSignalSlidedown;
+// 	Notifications: IOneSignalNotifications;
+// 	Session: IOneSignalSession;
+// 	User: IOneSignalUser;
+// 	Debug: IOneSignalDebug;
+// 	login(externalId: string, jwtToken?: string): Promise<void>;
+// 	logout(): Promise<void>;
+// 	init(options: IInitObject): Promise<void>;
+// 	setConsentGiven(consent: boolean): Promise<void>;
+// 	setConsentRequired(requiresConsent: boolean): Promise<void>;
+// }
