67313853a18f966a2604e49ac008f3f20bc4d983
MamunAG <52345044+MamunAG@users.noreply.github.com>
2024-11-05 00:56:49 +0600

    Update auth.ts

--- auth.ts
+++ auth.ts
@@ -12,6 +12,9 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
             const isLoggedIn = !!auth?.user;
             const { pathname } = nextUrl;
           //  const role = auth?.user.role || 'user';
+          if (isLoggedIn) {
+                return Response.redirect(new URL('/', nextUrl));
+            }
             if (pathname.startsWith('/api/auth/signin') && isLoggedIn) {
                 return Response.redirect(new URL('/', nextUrl));
             }
