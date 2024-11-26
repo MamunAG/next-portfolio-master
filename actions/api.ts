d1555b2d8d635bc1c3b2a35dd33a629fb4a7451a
MamunOfficeLaptop <mamun166036@gmail.com>
2024-11-26 23:03:08 +0600

    ppp

--- actions/api.ts
+++ actions/api.ts
@@ -1,2 +1,2 @@
-// export const root = "https://kkkadmin.vercel.app/api";
-export const root = "http://localhost:3001/api";
+export const root = "https://kkkadmin.vercel.app/api";
+//export const root = "http://localhost:3001/api";
